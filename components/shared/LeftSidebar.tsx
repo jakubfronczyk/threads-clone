d063f814115a7b9749d1d1ee02868de06cc3016d
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-09-09 10:50:04 +0200

    add styling for layout

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { sidebarLinks } from '@/constants'
+import { SignedIn, SignOutButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter, usePathname } from 'next/navigation'
@@ -17,12 +18,28 @@ function LeftSidebar() {
                         (pathname.includes(link.route) && link.route, length > 1) ||
                         pathname === link.route
                     return (
-                        <Link href={link.route} key={link.label} className="leftsidebar_link">
+                        <Link
+                            href={link.route}
+                            key={link.label}
+                            className={`leftsidebar_link ${isActive ? 'bg-primary-500' : ''}`}
+                        >
                             <Image src={link.imgURL} alt={link.label} width={24} height={24} />
                             <p className="text-light-1 max-lg:hidden">{link.label}</p>
                         </Link>
                     )
                 })}
+            </div>
+
+            <div className="mt-10 px-6">
+                <SignedIn>
+                    <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+                        <div className="flex cursos-pointer gap-4 p-4">
+                            <Image src="/assets/logout.svg" width={24} height={24} alt="logout" />
+
+                            <p className="text-light-2 max-lg:hidden">Logout</p>
+                        </div>
+                    </SignOutButton>
+                </SignedIn>
             </div>
         </section>
     )
