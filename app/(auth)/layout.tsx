e5abdeffa1e7fdc37b35fac484a8ecdb2b734298
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-09-09 08:52:28 +0200

    implement clerk auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "../globals.css";
+
+export const metadata: Metadata = {
+    title: "Threads Clone",
+    description: "Theads Clone built by Jakub Fronczyk",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <ClerkProvider>
+            <html lang="eng">
+                <body className={`${inter.className} bg-black`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    );
+}
